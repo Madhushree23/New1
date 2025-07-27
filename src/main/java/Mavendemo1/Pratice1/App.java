485f3822915ba95cd471621eb903d42cac4c8189
Madhushree <madhushree91102@gmail.com>
2025-07-27 23:25:20 +0530

    Removed submodule and added src files correctly

---
+++ src/main/java/Mavendemo1/Pratice1/App.java
@@ -0,0 +1,10 @@
+package Mavendemo1.Pratice1;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Mavendemo1/Pratice1/MyDemo.java
@@ -0,0 +1,25 @@
+package Mavendemo1.Pratice1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//THIS IS BY MADHU
+public class MyDemo {
+	
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			WebDriver driver=new ChromeDriver();
+			driver.get("https://tutorialsninja.com/demo/");
+			driver.manage().window().maximize();
+			//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			driver.findElement(By.xpath("//span[text()='My Account']")).click();
+			driver.findElement(By.linkText("Login")).click();
+			driver.findElement(By.id("input-email")).sendKeys("hades@gmail.com");
+			driver.findElement(By.id("input-password")).sendKeys("Hades@23");
+			driver.findElement(By.xpath("//input[@class=\"btn btn-primary\"]")).click();
+		}
+
+	}
+
